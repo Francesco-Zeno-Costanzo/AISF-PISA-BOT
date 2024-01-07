9009773ef41dc364d10e35b3cb6279145b0bc597
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2024-01-07 14:01:19 +0100

    Codice ausiliario per il riavvio

---
+++ run.py
@@ -0,0 +1,23 @@
+"""
+Codice per gestire il riavvio del bot
+"""
+import numpy as np
+from subprocess import call
+
+while True:
+    
+    # Leggo il messaggio lasciato dal bot
+    n = np.loadtxt("start.txt", unpack=True)
+    n = int(n)
+    
+    # Se è zero il bot è spento e va riaccesso
+    if n == 0:
+        
+        # Cambio il testo per sapere che il bot è acceso
+        f = open("start.txt", "w")
+        f.write("1 \n")
+        f.close()
+        
+        # Avvio il bot
+        call("python3 bot.py", shell=True)
+        # IL ciclo non riparte finchè il bot non viene nuovamente killato
