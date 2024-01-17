57bb5f506ff3d9a9170cb3f0c7f86e7560002eac
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2024-01-17 16:02:01 +0100

    piccoli cambiamenti

--- bot.py
+++ bot.py
@@ -509,7 +509,7 @@ async def echo(update:Update, context:ContextTypes.DEFAULT_TYPE):
             await context.bot.send_message(chat_id=chat_id, text=description)
         
         # Se il primo carattere corrisponde ma il resto no
-        elif not update.message.text in All and len(All)>1:
+        elif not update.message.text in All and not update.message.text.isalpha():
             description = "Non trovo una corrispondenza, attento in caso a scrivere bene il codice"
             await context.bot.send_message(chat_id=chat_id, text=description)
         
